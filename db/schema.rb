93dac13939ac8dbc69d14718aaea492eb6c0b1d9
shinya-dtn <majin.beesan@gmail.com>
2020-09-30 12:44:39 +0900

    commit user

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 2020_09_29_020448) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_k", null: false
+    t.string "last_name_k", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

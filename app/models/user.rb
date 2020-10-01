db87048cdb8927ccb89d519c0a8cb5c94668bb14
shinya-dtn <majin.beesan@gmail.com>
2020-10-01 11:14:19 +0900

    新規登録機能のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validats :nickname
+    validats :first_name, :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validats :first_name_k, :last_name_k, format: {with: /\A[ァ-ン]+\z/}
+    validats :birthday, 
+  end
 end

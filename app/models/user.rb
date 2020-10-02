b71af78391eed1f152950121a747d5f1e805e278
shinya-dtn <majin.beesan@gmail.com>
2020-10-02 11:27:15 +0900

    Rubocop実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :first_name, :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
-    validates :first_name_k, :last_name_k, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name_k, :last_name_k, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
 end

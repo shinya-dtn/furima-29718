63e967cca0b42da843039d4f4d7ea7b5e5bea45f
shinya-dtn <majin.beesan@gmail.com>
2020-10-03 16:07:39 +0900

    ログインユーザーのみ出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,22 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :move_to_index, except: [:index]
+  def index  
   end
 
-  def new
+  def new  
   end
 
   def create
   end
 
   def delete
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end

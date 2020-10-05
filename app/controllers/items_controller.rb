5acf1e8d4b8d3f4e2df67e62e35dbfaae842369e
shinya-dtn <majin.beesan@gmail.com>
2020-10-05 18:37:52 +0900

    商品の出品データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,15 +4,32 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save 
+      redirect_to root_path
+    else
+      render new_item_path
+    end
   end
 
   def delete
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  
   private
+
+  def item_params
+    params.require(:item).permit(:image, :prodact_name, :comment, :category_id, :status_id, :burden_id, :area_id, :day_id, :money).merge(user_id: current_user.id)
+  end
 
   def move_to_index
     unless user_signed_in?

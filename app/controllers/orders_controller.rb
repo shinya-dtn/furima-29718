89de15d4f04a084b0d68e1691370cc654f194451
shinya-dtn <majin.beesan@gmail.com>
2020-10-11 11:46:41 +0900

    ordersコントローラーのindexビュー表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, expect: [:index]
+  def index
+    @item = Item.find(params[:item_id])
+  end
+  private
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
+  
   belongs_to :user
   belongs_to :item
   has_one :address
+  has_one_attached :image
 end
